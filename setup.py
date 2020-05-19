8d02d8fd624abccb48683fe21218665f14dde440
Syren99 <p.rafaelalves@gmail.com>
2020-05-19 12:44:47 +0100

    Second Commit

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
-setup(name='PedroAlves',
+setup(name='steamed_hams',
       version='1.0',
       description='PedroAlves Distribution Module',
       author='Your Name',
       author_email='Your Email',
       url='The URL project - github for example',
-      py_modules=['PedroAlves'],
+      py_modules=['steamed_hams'],
      )
